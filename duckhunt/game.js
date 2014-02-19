4f21da38f8e5d5a49ffb09cb29e2d14e755229fd
Rob Lasell <roblasell@gmail.com>
2014-02-19 05:32:23 -0500

    added sprites and draw() to game.js

---
+++ duckhunt/game.js
@@ -0,0 +1,34 @@
+function draw() {
+	var canvas = document.getElementById('game');
+	if (canvas.getContext) {
+		var ctx = canvas.getContext('2d');
+		var sprite_sheet = new Image();
+		sprite_sheet.src = "assets/duckhunt.png";
+		sprite_sheet.onload = function() {
+			ctx.fillStyle = "#87CEEB";
+			ctx.fillRect(0, 0, 800, 500);
+
+			//ctx.drawImage(image, x, y, w, h, x2, y2, w2, h2);
+
+			//tree
+			ctx.drawImage(sprite_sheet, 0, 270, 75, 150, 0, 50, 250, 500);
+
+			//ground
+			ctx.fillStyle = "#C96A1B";
+			ctx.fillRect(0, 550, 800, 50);
+			ctx.drawImage(sprite_sheet, 0, 600, 800, 300, 0, 250, 800, 300);
+
+			//doge
+			ctx.drawImage(sprite_sheet, 0, 0, 60, 50, 230, 440, 162, 135);
+
+			//birds
+			ctx.drawImage(sprite_sheet, 0, 120, 40, 30, 200, 70, 68, 51);
+			ctx.drawImage(sprite_sheet, 205, 120, 40, 30, 500, 100, 68, 51);
+			ctx.drawImage(sprite_sheet, 300, 157, 40, 30, 260, 200, 68, 51);
+			ctx.drawImage(sprite_sheet, 300, 120, 40, 30, 270, 110, 68, 51);
+			ctx.drawImage(sprite_sheet, 40, 120, 40, 30, 600, 200, 68, 51);
+		}.bind(this);
+	} else {
+		alert('Sorry, canvas not supported by your browser!');
+	}
+}
